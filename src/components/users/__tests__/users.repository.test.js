194a900f4e6214b5a409f83cc7540662ec8ef7ff
vipin7 <vipinchackomdy@gmail.com>
2021-07-30 12:16:28 +0530

    Add tests for users repository

---
+++ src/components/users/__tests__/users.repository.test.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const { getUserByEmail, addUser } = require('../users.repository');
+const { User } = require('../../../models/User');
+
+jest.mock('../../../models/User');
+
+describe('Users repository', () => {
+  describe('getUserByEmail', () => {
+    it('calls findOne method on User model passing an object with email as key', async () => {
+      User.findOne.mockImplementation(jest.fn());
+
+      const email = 'john@testmail.com';
+
+      await getUserByEmail(email);
+
+      expect(User.findOne).toHaveBeenCalledWith({ email });
+    });
+  });
+
+  describe('addUser', () => {
+    it('should create a new object of the User model', async () => {
+      var username = 'John';
+      var email = 'john@testmail.com';
+      var password = 'password';
+
+      User.mockImplementation(() => {
+        var user = {
+          save: () => {
+            user.id = '123';
+          },
+        };
+        return user;
+      });
+
+      const newUser = await addUser(username, email, password);
+
+      expect(User).toHaveBeenCalledTimes(1);
+      expect(newUser).toEqual({ id: '123' });
+    });
+  });
+});
